85a45bcdd04c0a81bdbbb26f0a7bcab5efc15293
Kamakshi Bhat <kamakshi@appsmith.com>
2024-05-20 12:42:27 +0000

    ..

---
+++ pages/Page3/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,15 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	const workbook = new ExcelJS.Workbook();
+		return 
+
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
